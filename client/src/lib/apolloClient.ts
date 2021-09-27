9ff1e9096426c1cbe9a25a39f043a490254cc943
TuanPham1604 <tuan.pham@watasolutions.com>
2021-09-27 09:59:15 +0700

    update for deploying to heroku

--- client/src/lib/apolloClient.ts
+++ client/src/lib/apolloClient.ts
@@ -48,11 +48,10 @@ function createApolloClient(headers: IncomingHttpHeaders | null = null) {
 	}
 
 	const httpLink = new HttpLink({
-		uri: 'http://localhost:4000/graphql', // Server URL (must be absolute)
-		// uri:
-		// 	process.env.NODE_ENV === 'production'
-		// 		? 'https://sleepy-castle-81019.herokuapp.com/graphql'
-		// 		: 'http://localhost:4000/graphql',
+		uri:
+			process.env.NODE_ENV === 'production'
+				? 'https://floating-taiga-99357.herokuapp.com/graphql'
+				: 'http://localhost:4000/graphql',
 		credentials: 'include', // Additional fetch() options like `credentials` or `headers`
 		fetch: enhancedFetch
 	})
